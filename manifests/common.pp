854e4e72e21ad1ed4a6d553a17ad7bcd6b830e54
Thomas Hays <git@thays.com>
2015-08-11 13:01:42 -0500

    Workaround for  difference between CentOS 7 and OL 7

--- manifests/common.pp
+++ manifests/common.pp
@@ -46,11 +46,16 @@ class ossec::common {
       }
 
       # Set up OSSEC repo
+      if( $releasever == "7Server" ) {
+        $local_releasever = "7"
+      } else {
+        $local_releasever = $releasever
+      }
       yumrepo { 'ossec':
         descr      => 'CentOS / Red Hat Enterprise Linux $releasever - ossec.net',
         enabled    => true,
         gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY.ossec.txt',
-        mirrorlist => 'http://updates.atomicorp.com/channels/mirrorlist/ossec/centos-$releasever-$basearch',
+        mirrorlist => 'http://updates.atomicorp.com/channels/mirrorlist/ossec/centos-$local_releasever-$basearch',
         priority   => 1,
         protect    => false,
       }
